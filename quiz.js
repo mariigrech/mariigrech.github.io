a3249cc70953f4a2a7888bfe63e6c95128d9f4ad
Maria Grech <33154793+mariigrech@users.noreply.github.com>
2019-04-21 20:27:29 +0200

    Trying out bootstrap approach

--- quiz.js
+++ quiz.js
@@ -1,107 +1,40 @@
-// select all elements
-const start = document.getElementById("start");
-const quiz = document.getElementById("quiz");
-const question = document.getElementById("question");
-const qImg = document.getElementById("qImg");
-const choiceA = document.getElementById("A");
-const choiceB = document.getElementById("B");
-const choiceC = document.getElementById("C");
-const choiceD = document.getElementById("D");
-const progress = document.getElementById("progress");
-const scoreDiv = document.getElementById("scoreContainer");
+$(function(){
+    var loading = $('#loadbar').hide();
+    $(document)
+    .ajaxStart(function () {
+        loading.show();
+    }).ajaxStop(function () {
+    	loading.hide();
+    });
 
-// create our questions
-let questions = [
-    {
-        question : "What does Maria stand for?",
-        imgSrc : "img/html.png",
-        choiceA : "duda",
-        choiceB : "blabla",
-        choiceC : "Wrong",
-        choiceD : "Wrong",
+    showQuestion(1, "Choose a colour:", "Red", "Blue", "Green", "Yellow");
 
-    },{
-        question : "What does CSS stand for?",
-        imgSrc : "img/css.png",
-        choiceA : "Wrong",
-        choiceB : "Correct",
-        choiceC : "Wrong",
+    $("label.btn").on('click',function () {
+    	var chosenAnswer = $(this).find('input:radio').val();
+    	$('#loadbar').show();
+    	$('#quiz').fadeOut();
+    	setTimeout(function(){
+          switch($("#questionNumber").val()) {
+            case '1':
+              showQuestion(2, 'Choose a random number:', '7', '5', '1', '6');
+              break;
+            case '2':
+              showQuestion(3, 'What type of animal do you identify yourself as?', 'Polar Bear', 'Cat', 'Owl', 'Dog');
+              break;
+            default:
+              break;
+          }
+          $('#quiz').show();
+          $('#loadbar').fadeOut();
+    	}, 1500);
+    });
+});
 
-    },{
-        question : "What does JS stand for?",
-        imgSrc : "img/js.png",
-        choiceA : "Wrong",
-        choiceB : "Wrong",
-        choiceC : "Correct",
-    }
-];
-
-// create some variables
-
-const lastQuestion = questions.length - 1;
-let runningQuestion = 0;
-let count = 0;
-const questionTime = 10; // 10s
-const gaugeWidth = 150; // 150px
-const gaugeUnit = gaugeWidth / questionTime;
-let TIMER;
-let score = 0;
-
-// render a question
-function renderQuestion(){
-    let q = questions[runningQuestion];
-
-    question.innerHTML = "<p>"+ q.question +"</p>";
-    qImg.innerHTML = "<img src="+ q.imgSrc +">";
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
-    choiceD.innerHTML = q.choiceD;
-}
-
-start.addEventListener("click",startQuiz);
-
-// start quiz
-function startQuiz(){
-    start.style.display = "none";
-    renderQuestion();
-    quiz.style.display = "block";
-    renderProgress();
-}
-
-// render progress
-function renderProgress(){
-    for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
-        progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
-    }
-}
-
-// checkAnwer
-
-function checkAnswer(answer){
-    if( answer == questions[runningQuestion].correct){
-        // answer is correct
-        score++;
-        // change progress color to green
-        answerIsCorrect();
-    }else{
-        // answer is wrong
-        // change progress color to red
-        answerIsWrong();
-    }
-    count = 0;
-    if(runningQuestion < lastQuestion){
-        runningQuestion++;
-        renderQuestion();
-    }
-}
-
-// answer is correct
-function answerIsCorrect(){
-    document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
-}
-
-// answer is Wrong
-function answerIsWrong(){
-    document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
+function showQuestion(number, title, answer1, answer2, answer3, answer4) {
+  $("#questionNumber").val(number);
+  $("#questionTitle").html(title);
+  $("#quiz > label:nth-child(2)").html(answer1);
+  $("#quiz > label:nth-child(3)").html(answer2);
+  $("#quiz > label:nth-child(4)").html(answer3);
+  $("#quiz > label:nth-child(5)").html(answer4);
 }
